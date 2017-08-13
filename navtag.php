3015deb7a89def894d401e849bc2395cc64aab0b
Arjun-008 <arjun.861996@gmail.com>
2017-08-13 19:30:52 +0530

    Add files via upload

---
+++ navtag.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="homepage.php"><p style="font-family: lucida console; font-weight: bold;">Food 'n' Fitness</p></a>
+    </div>
+    <ul class="nav navbar-nav">
+      <!--<li class="active"><a href="homepage.php">Home</a></li>-->
+      <li><a href="about.php">About Us</a></li>
+	  <li><a href="work.php">Work With Us</a></li>
+      <li><a href="contact.php">Contact Us</a></li>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </ul>
+    <form class="navbar-form navbar-left">
+      <div class="input-group">
+        
+        <input type="text" class="form-control" placeholder="Search">
+        <div class="input-group-btn">
+          <button class="btn btn-default" type="submit">
+            <i class="glyphicon glyphicon-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+	<ul class="nav navbar-nav navbar-right">
+      <li><a href="reg.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Log In</a></li>
+    </ul>
+  </div>
+</nav>
+
+</body>
+</html>
+
+
+
